c572aafeaa94fdba64db20af985e08583cc140e2
showduhtung <shaun.tung@gmail.com>
2018-11-13 17:48:38 -0500

    added the first functioning map on home page on login

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
+import ReactMapGL from 'react-map-gl'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+// const UserHome = props => {
+//   const {email} = props
+
+//   return (
+//     <div>
+//       <h3>Welcome, {email}</h3>
+//     </div>
+//   )
+// }
+
+class UserHome extends React.Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 40.84013,
+      longitude: -73.938001,
+      zoom: 16
+    }
+  }
+
+  render() {
+    console.log(this)
+    return (
+      <div>
+        Welcome, {this.props.email}
+        <ReactMapGL
+          mapboxApiAccessToken="pk.eyJ1Ijoic2xlZXB5a2lkMzYiLCJhIjoiY2pvZ2F0bjlhMGQ0cTNwbjBnZzI5em01aCJ9.DNsguoMme8AGCqtmKWiJ0w"
+          {...this.state.viewport}
+          onViewportChange={viewport => this.setState({viewport})}
+        />
+      </div>
+    )
+  }
 }
 
 /**
